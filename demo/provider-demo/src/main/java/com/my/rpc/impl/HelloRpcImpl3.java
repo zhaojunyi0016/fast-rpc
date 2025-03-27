9cad499a65af28add2bc20a38733cbf3cc215942
zhaojunyi <451108902@qq.com>
2025-03-27 09:25:08 +0800

    修改

--- demo/provider-demo/src/main/java/com/my/rpc/impl/HelloRpcImpl3.java
+++ demo/provider-demo/src/main/java/com/my/rpc/impl/HelloRpcImpl3.java
@@ -9,6 +9,6 @@ import com.my.rpc.SayHelloRpc3;
 public class HelloRpcImpl3 implements SayHelloRpc3 {
     @Override
     public String sayHi(String msg) {
-        return "1111221 + consumer:" + msg;
+        return "111122111221 + consumer:" + msg;
     }
 }
