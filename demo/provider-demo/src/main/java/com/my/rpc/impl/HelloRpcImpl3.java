0ebeb3ffd09c15d24c0d03196b8aa691c4b02ea2
zhaojunyi <451108902@qq.com>
2025-04-25 09:24:23 +0800

    1212

--- demo/provider-demo/src/main/java/com/my/rpc/impl/HelloRpcImpl3.java
+++ demo/provider-demo/src/main/java/com/my/rpc/impl/HelloRpcImpl3.java
@@ -9,6 +9,6 @@ import com.my.rpc.SayHelloRpc3;
 public class HelloRpcImpl3 implements SayHelloRpc3 {
     @Override
     public String sayHi(String msg) {
-        return "123 + consumer:" + msg;
+        return "1231212 + consumer:" + msg;
     }
 }
